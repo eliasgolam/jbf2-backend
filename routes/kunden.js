52cb10e63524aa197a9c9ded1b811eeaed61280e
Elias Golam <elias.golam@jbfinanz.ch>
2025-05-01 15:29:54 +0200

    Update: CORS-Methoden oder Logging in server.js angepasst

--- routes/kunden.js
+++ routes/kunden.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const Kunde = require('../models/Kunde');
 
-// Kunde erstellen (POST)
 router.post('/', async (req, res) => {
   try {
     const neuerKunde = new Kunde({
